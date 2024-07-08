792d041ce82d0423aa02c0ab55cce627359ecdf3
Alexandre Idziak <101719176+DreamoreWM@users.noreply.github.com>
2024-07-08 15:07:44 +0200

    review cypress

--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,22 +1,6 @@
 // cypress/integration/navigation.spec.js
 
 describe('Navigation Test', () => {
-    it('Vérifie les boutons de navigation pour un utilisateur non connecté', () => {
-        cy.visit('/dashboard'); // Remplacez par l'URL de votre tableau de bord
-
-        cy.get('nav.navbar.navbar-transparent').should('have.css', 'opacity', '1');
-
-        // Vérifie que les boutons pour un utilisateur non connecté sont présents
-        cy.contains('Prendez rendez-vous').should('be.visible');
-        cy.contains('Créer un compte').should('be.visible');
-        cy.contains('Se connecter').should('be.visible');
-
-        // Vérifie que les boutons pour un utilisateur connecté ne sont pas présents
-        cy.contains('Déconnexion').should('not.exist');
-        cy.contains('Coiffeurs').should('not.exist');
-        cy.contains('Prestations').should('not.exist');
-    });
-
     it('Vérifie les boutons de navigation pour un utilisateur connecté', () => {
         // Simule une connexion utilisateur (utilisez une commande personnalisée ou API pour vous connecter)
         cy.login('test@test.test', 'test123!'); // Implémentez la commande `cy.login`
